5950491209f0d53f2ea375547f44bc1b081ba761
kennedy-ben <makuanikennedy@gmail.com>
2024-02-11 15:16:43 +0300

    Created landing page and added syles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import "./index.css";
+// import loginImage from "../assets/login-image.jpg";
+import loginImage from "../assets/login-image.jpg";
 
-export default function index() {
+export default function Index() {
   return (
     <div>
-    <Header/>
-      <p>This is where landing page must be.</p>
+      <section className="login-section">
+        <div className="login-content">
+          <div className="login-image">
+            <img src={loginImage} alt="Login" />
+           
+          </div>
+          <div className="login-form">
+            <h2>Login to Your Account</h2>
+            <form>
+              <input type="text" placeholder="Username" />
+              <input type="password" placeholder="Password" />
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" />
+                  Remember me
+                </label>
+                <a href="#" className="forgot-password">
+                  Forgot password?
+                </a>
+              </div>
+              <button type="submit" className="submit-button">
+                Login
+              </button>
+              <div className="or-divider">or</div>
+              <button type="button" className="google-signup">
+                Sign up with Google
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
