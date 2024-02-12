4a1ccb6b2fca20faf4e67e8b7f985725c464b65b
kennedy-ben <makuanikennedy@gmail.com>
2024-02-12 14:10:51 +0300

    multiple styles implemented on random folder

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -17,7 +17,24 @@ function Header() {
         <a href="/state">Users</a>
         <a href="/Album/:albumId">Albums</a>
         <a href="/Photo">Photos</a>
-        <button onClick={handleLogout}>logout</button>
+        <button
+          onClick={handleLogout}
+          style={{
+            backgroundColor: "##333" /* Green */,
+            border: "none",
+            color: "black",
+            padding: "10px 20px",
+            textAlign: "center",
+            textDecoration: "none",
+            display: "inline-block",
+            fontSize: "16px",
+            margin: "4px 2px",
+            cursor: "pointer",
+            borderRadius: "4px",
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
