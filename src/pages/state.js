be3e71770860da88880beb366a59b68cca23dd18
kennedy-ben <makuanikennedy@gmail.com>
2024-02-14 12:12:53 +0300

    modified state page which is user page

--- src/pages/state.js
+++ src/pages/state.js
@@ -4,47 +4,69 @@ import Header from "./Header";
 import "./state.css";
 
 export default function State() {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
+    setIsLoading(true); 
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((resp) => resp.json())
       .then((usersData) => {
-        usersData.forEach((user) => {
-          fetch(`https://jsonplaceholder.typicode.com/albums?userId=${user.id}`)
-            .then((resp) => resp.json())
-            .then((albumsData) => {
-              user.albumCount = albumsData.length;
-              setUsers([...usersData]);
-            });
-        });
+        const fetchAlbumCount = async (user) => {
+          const albumsResp = await fetch(
+            `https://jsonplaceholder.typicode.com/albums?userId=${user.id}`
+          );
+          const albumsData = await albumsResp.json();
+          user.albumCount = albumsData.length;
+          return user;
+        };
+
+        const fetchAllAlbumCounts = async () => {
+          const updatedUsers = await Promise.all(
+            usersData.map((user) => fetchAlbumCount(user))
+          );
+          setUsers(updatedUsers);
+        };
+
+        fetchAllAlbumCounts();
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => {
+        setIsLoading(false); 
       });
   }, []);
+
   return (
     <div>
       <Header />
       <div className="container">
         <h1>Users Albums</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>User</th>
-              <th>Albums Count</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>
-                  <Link to={`/user/${user.id}`}>{user.name}</Link> {}
-                </td>
-                <td>{user.albumCount || 0}</td>
+        {isLoading ? ( 
+          <div className="loader"></div>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>User</th>
+                <th>Albums Count</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>
+                    <Link to={`/user/${user.id}`}>{user.name}</Link> {}
+                  </td>
+                  <td>{user.albumCount || 0}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
 }
- 
